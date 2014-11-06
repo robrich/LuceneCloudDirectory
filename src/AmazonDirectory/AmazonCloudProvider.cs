892f3066fefa7d31e6562ca9f83296e83d3e4dac
Julio Spader <julio.spader@vetorsistemas.com.br>
2014-11-06 11:44:33 -0200

    Updated to use Amazon SDK 2.3.5.0

--- src/AmazonDirectory/AmazonCloudProvider.cs
+++ src/AmazonDirectory/AmazonCloudProvider.cs
@@ -11,13 +11,16 @@
     using global::Amazon.S3;
     using global::Amazon.S3.Model;
 
-    public class AmazonCloudProvider : ICloudProvider
+    public class AmazonCloudProvider : ICloudProvider, IDisposable
     {
         private readonly string amazonKey;
         private readonly string amazonSecret;
         private readonly string amazonBucket;
+        private readonly RegionEndpoint amazonRegion;
 
-        public AmazonCloudProvider(string AmazonKey, string AmazonSecret, string AmazonBucket)
+        public IAmazonS3 clientAmazon { get; private set; }
+
+        public AmazonCloudProvider(string AmazonKey, string AmazonSecret, string AmazonBucket, string AmazonRegion)
         {
             if (string.IsNullOrEmpty(AmazonKey))
             {
@@ -31,6 +34,20 @@
             {
                 throw new ArgumentNullException("AmazonBucket");
             }
+            if (string.IsNullOrEmpty(AmazonRegion))
+            {
+                throw new ArgumentNullException("AmazonRegion");
+            }
+            else
+            {
+                var amazonRegion = RegionEndpoint.EnumerableAllRegions.Where(w => w.SystemName == AmazonRegion).FirstOrDefault();
+
+                if (null == amazonRegion)
+                    throw new ArgumentOutOfRangeException("AmazonRegion not found");
+             
+                this.amazonRegion = amazonRegion;
+            }
+
             this.amazonKey = AmazonKey;
             this.amazonSecret = AmazonSecret;
             this.amazonBucket = AmazonBucket;
@@ -38,14 +55,11 @@
 
         public void InitializeStorage()
         {
-            // If these error with things like invalid credentials, now you know
-            using (AmazonS3 client = AWSClientFactory.CreateAmazonS3Client(this.amazonKey, this.amazonSecret))
+            try
             {
+                this.clientAmazon = AWSClientFactory.CreateAmazonS3Client(this.amazonKey, this.amazonSecret, this.amazonRegion);
 
-                ListBucketsRequest request = new ListBucketsRequest
-                {
-                };
-                ListBucketsResponse response = client.ListBuckets(request);
+                ListBucketsResponse response = this.clientAmazon.ListBuckets();
 
                 List<S3Bucket> buckets = response.Buckets;
                 bool exists = (
@@ -56,47 +70,52 @@
 
                 if (!exists)
                 {
-                    PutBucketResponse bucketResponse = client.PutBucket(new PutBucketRequest
+                    PutBucketResponse bucketResponse = this.clientAmazon.PutBucket(new PutBucketRequest
                     {
-                        BucketName = this.amazonBucket
+                        BucketName = this.amazonBucket,
+                        BucketRegion = this.amazonRegion.SystemName
                     });
                 }
             }
+            catch (AmazonS3Exception)
+            {
+                throw;
+            }
         }
+
         public List<string> ListAll()
         {
-
             List<string> files = new List<string>();
-            using (AmazonS3 client = AWSClientFactory.CreateAmazonS3Client(this.amazonKey, this.amazonSecret))
+
+            ListObjectsRequest request = new ListObjectsRequest
             {
+                BucketName = this.amazonBucket
+            };
 
-                ListObjectsRequest request = new ListObjectsRequest
+            do
+            {
+                ListObjectsResponse response = this.clientAmazon.ListObjects(request);
+
+                foreach (S3Object s3Object in response.S3Objects)
                 {
-                    BucketName = this.amazonBucket
-                };
-                do
+                    files.Add(s3Object.Key);
+                }
+
+                // If response is truncated, get the next page
+                if (response.IsTruncated)
                 {
-                    ListObjectsResponse response = client.ListObjects(request);
+                    request.Marker = response.NextMarker;
+                }
+                else
+                {
+                    request = null;
+                }
+            } while (request != null);
 
-                    foreach (S3Object s3Object in response.S3Objects)
-                    {
-                        files.Add(s3Object.Key);
-                    }
-
-                    // If response is truncated, get the next page
-                    if (response.IsTruncated)
-                    {
-                        request.Marker = response.NextMarker;
-                    }
-                    else
-                    {
-                        request = null;
-                    }
-                } while (request != null);
-            }
 
             return files;
         }
+
         // Always returns an object even if file doesn't exist
         public FileMetadata FileMetadata(string name)
         {
@@ -104,129 +123,118 @@
             {
                 Name = name
             };
-            using (AmazonS3 client = AWSClientFactory.CreateAmazonS3Client(this.amazonKey, this.amazonSecret))
+
+            try
             {
+                GetObjectMetadataResponse response = this.clientAmazon.GetObjectMetadata(this.amazonBucket, name);
 
-                GetObjectMetadataRequest request = new GetObjectMetadataRequest
-                {
-                    BucketName = this.amazonBucket,
-                    Key = name
-                };
-
-                try
-                {
-                    GetObjectMetadataResponse response = client.GetObjectMetadata(request);
-
-                    results.Exists = true; // else AWSSDK threw
-                    results.Length = response.ContentLength;
-                    results.LastModified = response.LastModified;
-
-                }
-                catch (AmazonS3Exception ex)
-                {
-                    if (ex.ErrorCode == "NoSuchKey")
-                    {
-                        results.Exists = false; // File doesn't exist
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-
+                results.Exists = true; // else AWSSDK threw
+                results.Length = response.ContentLength;
+                results.LastModified = response.LastModified;
             }
+            catch (AmazonS3Exception ex)
+            {
+                if (ex.ErrorCode == "NoSuchKey" || ex.StatusCode == HttpStatusCode.NotFound)
+                {
+                    results.Exists = false; // File doesn't exist
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
             return results;
         }
+
         public void Delete(string name)
         {
-            using (AmazonS3 client = AWSClientFactory.CreateAmazonS3Client(this.amazonKey, this.amazonSecret))
+            DeleteObjectRequest request = new DeleteObjectRequest
             {
-                DeleteObjectRequest request = new DeleteObjectRequest
-                {
-                    BucketName = this.amazonBucket,
-                    Key = name
-                };
-                client.DeleteObject(request);
-            }
+                BucketName = this.amazonBucket,
+                Key = name
+            };
+            this.clientAmazon.DeleteObject(request);
+
             if (!name.EndsWith(".lock"))
             {
                 this.Delete(name + ".lock");
             }
         }
+
         public Stream Download(string name)
         {
-            using (AmazonS3 client = AWSClientFactory.CreateAmazonS3Client(this.amazonKey, this.amazonSecret))
+            GetObjectRequest request = new GetObjectRequest
             {
+                BucketName = this.amazonBucket,
+                Key = name
+            };
 
-                GetObjectRequest request = new GetObjectRequest
+            try
+            {
+                GetObjectResponse response = this.clientAmazon.GetObject(request);
+
+                MemoryStream ms = new MemoryStream();
+                response.ResponseStream.CopyTo(ms);
+                ms.Position = 0;
+
+                return ms;
+            }
+            catch (AmazonS3Exception ex)
+            {
+                if (ex.ErrorCode == "NoSuchKey" || ex.StatusCode == HttpStatusCode.NotFound)
                 {
-                    BucketName = this.amazonBucket,
-                    Key = name
-                };
-
-                try
-                {
-                    GetObjectResponse response = client.GetObject(request);
-
-                    MemoryStream ms = new MemoryStream();
-                    response.ResponseStream.CopyTo(ms);
-                    ms.Position = 0;
-
-                    return ms;
+                    return null; // File doesn't exist
                 }
-                catch (AmazonS3Exception ex)
+                else
                 {
-                    if (ex.ErrorCode == "NoSuchKey")
-                    {
-                        return null; // File doesn't exist
-                    }
-                    else
-                    {
-                        throw;
-                    }
+                    throw;
                 }
-
             }
         }
+
         public void Upload(string name, Stream content, FileMetadata FileMetadata)
         {
-            using (AmazonS3 client = AWSClientFactory.CreateAmazonS3Client(this.amazonKey, this.amazonSecret))
-            {
-                PutObjectRequest request = new PutObjectRequest
-                {
-                    BucketName = this.amazonBucket,
-                    Key = name,
-                    InputStream = content
-                };
-                client.PutObject(request);
-            }
+            PutObjectRequest request = new PutObjectRequest
+               {
+                   BucketName = this.amazonBucket,
+                   Key = name,
+                   InputStream = content
+               };
+            this.clientAmazon.PutObject(request);
         }
+
         public void Touch(string name)
         {
             // FRAGILE: Amazon doesn't support modifying the date
             // TODO: Download then re-upload it?
         }
+
         // FRAGILE: It's likely much less chatty to store locks in an external data store like Redis or SimpleDB http://stackoverflow.com/questions/3431418/locking-with-s3
         public bool ObtainLock(string name)
         {
             if (this.IsLocked(name))
-            {
                 return false;
-            }
+            
             using (MemoryStream ms = new MemoryStream())
-            {
                 this.Upload(name, ms, new FileMetadata());
-            }
+            
             return true;
         }
+
         public void Releaselock(string name)
         {
             this.Delete(name);
         }
+
         public bool IsLocked(string name)
         {
             return this.FileMetadata(name).Exists;
         }
 
+        public void Dispose()
+        {
+            this.clientAmazon.Dispose();
+        }
     }
 }
